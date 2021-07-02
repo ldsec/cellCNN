172ab5089d3c1f5791876d3248e015785fb869e5
sinemsav <sinemsav92@gmail.com>
2021-07-02 11:20:35 +0200

    small fix

--- cellCNNClear/protocols/common/parameters.go
+++ cellCNNClear/protocols/common/parameters.go
@@ -2,20 +2,20 @@ package common
 
 // cellCNN parameters
 const NCELLS = 200
-const NFEATURES = 16
-const NSAMPLES = 1490
-const NSAMPLES_DIST = 500
-const NCLASSES = 3
+const NFEATURES = 37
+const NSAMPLES = 1000
+const NSAMPLES_DIST = 997
+const NCLASSES = 2
 const NFILTERS = 8
-const DATA_FOLDER = "../../data/cellCNN/originalAML/"
-const SPLIT_DATA_FOLDER = "../../data/cellCNN/splitAML/"
-const ApproxInterval = 8.
-const testAllCell = 12440
-const BATCH_SIZE = 50
-const LEARN_RATE = 0.009
+const DATA_FOLDER = "../../data/cellCNN/originalNK/"
+const SPLIT_DATA_FOLDER = "../../data/cellCNN/splitNK/"
+const ApproxInterval = 3.
+const testAllCell = 5652
+const BATCH_SIZE = 100
+const LEARN_RATE = 0.01
 const MOMENTUM = 0.9
 
-const MICRO = true
+const MICRO = false
 
 type Loader interface {
 	Load() (CnnDataset, error)
